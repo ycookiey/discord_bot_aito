b5b9843320ea0b6fb6bc0623e501d18b23f4725c
cookie <yamamon137@gmail.com>
2024-03-06 17:02:23 +0900

    update:メンションされたときgemini APIを通して返答する

--- main.py
+++ main.py
@@ -2,13 +2,26 @@ import os
 import discord
 import re
 from activate_server import activate_server
+import google.generativeai as genai
+import textwrap
+from IPython.display import Markdown
 
 TOKEN = os.getenv("TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+BOT_ID = os.getenv("BOT_ID")
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel("gemini-pro")
+
+
+def to_markdown(text):
+    text = text.replace("•", "  *")
+    return Markdown(textwrap.indent(text, "> ", predicate=lambda _: True))
 
 
 @client.event
@@ -30,7 +43,8 @@ async def on_message(user_message):
         bot_message = "https://www.google.com/search?q=" + "+".join(
             re.split("[ 　]+", user_text[:-3].strip())
         )
-
+    elif client.user.mentioned_in(user_message):
+        bot_message = model.generate_content(user_text.replace(f"<@{BOT_ID}>", "")).text
     await user_message.reply(bot_message)
 
 
